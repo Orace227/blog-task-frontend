6cec485f73c8627bd42625bc2163378740505249
Orbit_Gamerz <gklibrary2020@gmail.com>
2024-05-18 17:13:19 +0530

    #6 added link in logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <header className="bg-blue-500 text-white py-4 ">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Blog Test</h1>
+        <Link to="/" className="hover:text-gray-300">
+          <h1 className="text-2xl font-bold">Blog Test</h1>
+        </Link>
         <nav>
           <ul className="flex space-x-4">
             <li>
